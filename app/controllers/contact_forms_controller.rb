8f1e9b970a0ca9d0ee6e3df3d11a531cae24c689
Luis Perichon <luigi@luigi-laptop.(none)>
2010-01-07 11:36:17 -0300

    style tuning by KmiShkti

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -1,8 +1,12 @@
 class ContactFormsController < InheritedResources::Base
   actions :new, :create
-
+  before_filter :page_title
   def create
     create!{ '/' }
+  end
+
+  def page_title
+    @page_title = 'Contacto'
   end
 
 end
