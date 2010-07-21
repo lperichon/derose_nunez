1474e1146dfc758a7affecf318c102f1a2d53bf1
Luis Perichon <luigi@luigi-laptop.(none)>
2010-07-21 17:27:17 -0300

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,12 +27,8 @@ config.action_view.cache_template_loading            = true
 # Enable threaded mode
 # config.threadsafe!
 
-config.action_mailer.delivery_method = :sendmail
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default_charset = "utf-8"
-config.action_mailer.sendmail_settings = {
-  :location => '/usr/sbin/sendmail',
-}
 
 HOST = "derosenuñez.com.ar"
