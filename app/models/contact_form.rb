a495cecdb45e57f064a3c0cfd49b1697cadffcb0
Luis Perichon <luisperichon@gmail.com>
2012-06-15 11:37:37 -0300

    temporarily change recipient for contact forms

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -1,6 +1,6 @@
 class ContactForm < SimpleForm
   subject "Contacto Web"
-  recipients "info@xn--derosenuez-09a.com.ar"
+  recipients "luisperichon@gmail.com"
   sender{|c| %{"#{c.name}" <#{c.email}>} }
 
   attribute :name,      :validate => true
